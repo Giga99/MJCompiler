ebe81a4ea39fe7092fb41945c78b4dd4c04ff5f5
Igor Stevanovic <igorstevanovic99@gmail.com>
2023-04-20 01:29:42 +0200

    Added SemanticAnalyzer base

--- test/rs/ac/bg/etf/pp1/MJTest.java
+++ test/rs/ac/bg/etf/pp1/MJTest.java
@@ -2,6 +2,7 @@ package rs.ac.bg.etf.pp1;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -13,6 +14,7 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
+import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
 
 public class MJTest {
@@ -37,11 +39,25 @@ public class MJTest {
 			MJParser p = new MJParser(lexer);
 			Symbol s = p.parse(); // Start of the parsing
 
+			Tab.init();
 			Program prog = (Program) (s.value);
 			
 			// Syntax tree print
 			log.info(prog.toString(""));
 			log.info("===================================");
+			
+			// Recognized program parts print
+			SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer();
+			prog.traverseBottomUp(semanticAnalyzer);
+			
+			log.info("===================================");
+			Tab.dump();
+			
+			if (!p.errorDetected && semanticAnalyzer.passed()) {
+				log.info("Parsing successfully completed!");
+			} else {
+				log.error("Parsing failed!");
+			}
 		} 
 		finally {
 			if (br != null) {
