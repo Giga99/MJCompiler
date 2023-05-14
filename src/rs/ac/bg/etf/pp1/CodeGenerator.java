5673c92441920b29166c9f33370919e0135d10c7
Igor Stevanovic <igorstevanovic99@gmail.com>
2023-05-14 15:01:37 +0200

    Added consts code generation

--- src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -2,6 +2,9 @@ package rs.ac.bg.etf.pp1;
 
 import org.apache.log4j.Logger;
 
+import rs.ac.bg.etf.pp1.ast.ConstValueBool;
+import rs.ac.bg.etf.pp1.ast.ConstValueChar;
+import rs.ac.bg.etf.pp1.ast.ConstValueNumber;
 import rs.ac.bg.etf.pp1.ast.MethodAnyReturnType;
 import rs.ac.bg.etf.pp1.ast.MethodDecl;
 import rs.ac.bg.etf.pp1.ast.MethodName;
@@ -13,8 +16,10 @@ import rs.ac.bg.etf.pp1.ast.SyntaxNode;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
 import rs.ac.bg.etf.pp1.helpers.Utils;
 import rs.ac.bg.etf.pp1.helpers.codegeneration.MethodCodeGenerationManager;
+import rs.ac.bg.etf.pp1.tabextended.TabExtended;
 import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
 
 public class CodeGenerator extends VisitorAdaptor {
 	private final int MAX_SOURCE_CODE_SIZE = 8192;
@@ -82,5 +87,26 @@ public class CodeGenerator extends VisitorAdaptor {
 		methodManager.setMethodHasReturn();
 		Code.put(Code.exit);
 		Code.put(Code.return_);
+	}
+	
+	public void visit(ConstValueNumber constValueNumber) {
+		Obj constant = Tab.insert(Obj.Con, "$", Tab.intType);
+		constant.setLevel(0);
+		constant.setAdr(constValueNumber.getValue());
+		Code.load(constant);
+	}
+	
+	public void visit(ConstValueChar constValueChar) {
+		Obj constant = Tab.insert(Obj.Con, "$", Tab.charType);
+		constant.setLevel(0);
+		constant.setAdr(constValueChar.getValue());
+		Code.load(constant);
+	}
+
+	public void visit(ConstValueBool constValueBool) {
+		Obj constant = Tab.insert(Obj.Con, "$", TabExtended.boolType);
+		constant.setLevel(0);
+		constant.setAdr(constValueBool.getValue().equals("true") ? 1 : 0);
+		Code.load(constant);
 	}
 }
